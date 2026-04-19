afb5b766fc6a075f9c151f71a7e0eb1062effddb
Gleywson Ribeiro <gleywson@gmail.com>
2026-04-18 21:09:27 -0300

    Ajuste do CORS

--- src/main/java/com/rainhacosmeticos/web/CorsConfig.java
+++ src/main/java/com/rainhacosmeticos/web/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "http://0.0.0.0:4200", "http://192.168.1.22:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);
